1ff13846040ba07259746b5e33463c75edd6115f
stli12 <143028001+stli12@users.noreply.github.com>
2024-04-16 11:35:01 -0700

    Update OnlinePurchaseManager.java

--- src/main/java/OnlinePurchaseManager.java
+++ src/main/java/OnlinePurchaseManager.java
@@ -15,7 +15,7 @@ public class OnlinePurchaseManager
     int count = 0;
    for(int i = 0; i < purchases.size(); i++){
 if(purchases.get(i).isElectronic() && purchases.get(i).getMaker().equals(maker))
-  count++
+  count++;
    }
     return count;
   }
